f121223ce297c9b61ca34e19bfdce5b59542eb2c
rickmaster2008 <ricardo.cotillo@gmail.com>
2021-02-06 18:17:03 -0500

    updated version

--- calc-financiera.php
+++ calc-financiera.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'CALC_FINANCIERA_VERSION', '1.0.6' );
+define( 'CALC_FINANCIERA_VERSION', '1.0.7' );
 
 /**
  * The code that runs during plugin activation.
