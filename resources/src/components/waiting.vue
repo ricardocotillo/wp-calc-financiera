a425d3e42b45cba90da7dc01d9e483a5f3257efb
rickmaster2008 <ricardo.cotillo@gmail.com>
2021-06-20 22:17:04 -0500

    fixed color in check svg

--- resources/src/components/waiting.vue
+++ resources/src/components/waiting.vue
@@ -7,12 +7,22 @@
           :src="`${baseUrl}/img/loader.svg`"
           class="w-10 mb-3"
         />
-        <img v-else :src="`${baseUrl}/img/check.svg`" class="w-10 mb-3" />
+        <!-- <img v-else :src="`${baseUrl}/img/check.svg`" class="w-10 mb-3" /> -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-10 mb-3"
+          :fill="primaryColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"
+          />
+        </svg>
       </div>
     </template>
     <template v-slot:body>
       <template v-if="loading">
-        <h1 class="text-lg text-blue-900 font-bold text-center max-w-md">
+        <h1 class="text-lg font-bold text-center max-w-md">
           Estamos procesando tu solicitud
         </h1>
         <p class="text-center max-w-md">
@@ -24,16 +34,17 @@
       <h1 v-else class="text-lg text-blue-900 font-bold text-center max-w-md">
         !Excelente!
       </h1>
-      <p class="text-center max-w-md">En las próximas horas un asesor especializado se comunicará
-        contigo para brindarte todos los detalles de tu solicitud.
+      <p class="text-center max-w-md">
+        En las próximas horas un asesor especializado se comunicará contigo para
+        brindarte todos los detalles de tu solicitud.
       </p>
     </template>
   </Modal>
 </template>
 
 <script>
-import Modal from "../layouts/modal";
-import { baseUrl } from "../mixins/calcData";
+import Modal from '../layouts/modal'
+import { baseUrl } from '../mixins/calcData'
 export default {
   props: {
     isOpen: {
@@ -48,10 +59,10 @@ export default {
   data() {
     return {
       baseUrl,
-    };
+    }
   },
   components: { Modal },
-};
+}
 </script>
 
 <style>
